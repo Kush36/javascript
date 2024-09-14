6c4f515e54773c5f17c946a024ec4f089d71f8d4
Kush36 <153112246+Kush36@users.noreply.github.com>
2024-09-14 17:08:43 +0000

    file of Datatypes and ECMA script,mdn

---
+++ 01JS_Topics/5_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";//treat all JS code as newer version
+// alert(3+3) //we are using node js not browser
+
+console.log(3+3)
+console.log("Kushagra")
+
+let name = "Kushagra Mishra"
+let age = 19
+let isloggedin = false
+let state;
+
+/*
+number => 2 ki power 53
+bigint => 
+string => ""
+boolean => true/false
+null => stand alone value
+undefined =>
+symbol => unique 
+
+object
+
+read documentation from ECMA script and mdn
+
+*/
+console.log(typeof "Kushagra Mishra");
+console.log(typeof state); //undefined
+console.log(typeof age);
+console.log(typeof null); //object
