6738d4f55524be228626fbb628a4180b899a0e03
Kush36 <153112246+Kush36@users.noreply.github.com>
2024-09-21 08:50:49 +0000

    Function01

---
+++ 05JS_Function/19_Function.js
@@ -0,0 +1,47 @@
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+/*
+H
+I
+T
+E
+S
+H
+*/
+
+sayMyName() // sayMyName is referrence  while () => is execusion of function
+
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+// addTwoNumbers() // o/p => NaN as () does not contain any parameter
+function addTwoNumbers(number1, number2){  // addTwoNumbers is referrence  while () => is execusion of function
+    //  console.log(number1 + number2); // M-1  =>8
+    // let result = number1 + number2 // anything given below return statement does not print but work above it 
+    // return result   // if you will not define result and printing result it will show undefined
+       return number1+number2 // above two line work in 1 line
+}
+// addTwoNumbers(3,5) //   M-1
+const result = addTwoNumbers(3, 5)  // 8
+
+console.log("Result: ", result); // Result:  8
+
+
+function loginUserMessage(username = "sam"){  // Username is parameter works only when no other parameter is their in below console function
+    if(!username){     
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Kush")) // if you do not pass any string it returnn undefined 
+// console.log(loginUserMessage("hitesh"))
