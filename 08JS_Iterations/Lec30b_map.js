c3e18345955ae85cb6c1997494fecefbf11fc63b
Kush36 <153112246+Kush36@users.noreply.github.com>
2024-10-09 19:28:31 +0000

    map

---
+++ 08JS_Iterations/Lec30b_map.js
@@ -0,0 +1,13 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
+
+// map can return a value without using return keyword  unlike filter
+// map can undergo chainning means map after map like in line 6,7,8
